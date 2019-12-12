3f33967e50104086ab83a430c564031076c9b562
Alan Verbner <alverbner@gmail.com>
2019-12-12 13:56:01 -0300

    docs: jSDocs added to install-deps script

--- install-deps.js
+++ install-deps.js
@@ -8,6 +8,13 @@ const fs = require('fs');
 
 const { PLATFORMS, platformsConfigs } = require('./platforms');
 
+/**
+ * This function uses Inquirer prompt to ask for:
+ *  - Platform
+ *  - If user want's to get `.eslintrc` file created
+ *
+ * @returns {Promise<Object>} Returns an object with two keys, platforms and createEslintrc
+ */
 const askUserForConfigs = () =>
   inquirer.prompt([
     {
@@ -30,6 +37,13 @@ const askUserForConfigs = () =>
     }
   ]);
 
+/**
+ * Launches an Ora spinner that will be shown while the function passed as parameter
+ * is executed. Once it's done it shows a succeed message, fail otherwise.
+ *
+ * @param  {String} message To be shown alongside the spinner
+ * @param  {Function<Promise<any>>} fn Function to be executed
+ */
 const withSpinner = async (message, fn) => {
   const spinner = ora(message).start();
 
@@ -41,6 +55,15 @@ const withSpinner = async (message, fn) => {
   }
 };
 
+/**
+ * Invokes npm to install the deps received as parameter. It will show a spinner
+ * while it executes the process.
+ *
+ * @param  {Array<String>} deps NPM compatible dependencies stated in any form
+ * supported by `npm install`.
+ * @see https://docs.npmjs.com/cli/install
+ * @returns {Promise<any>} a promise that will resolve once the deps are installed.
+ */
 const installDeps = deps =>
   withSpinner('Installing deps', async notify => {
     for (const dep of deps) {
@@ -53,12 +76,17 @@ const installDeps = deps =>
     }
   });
 
-const getPlatformDeps = platform => platformsConfigs[platform].deps;
-
+/**
+ * Writes `.erlintrc` file depending on the specified platforms in order to target
+ * configs stated in `configurations` folder.
+ *
+ * @param  {Array<String>} platforms platforms to be written in the `.eslintrc` file
+ */
 const writeEslintrc = platforms =>
   withSpinner(
     `Creating .eslintrc file for ${platforms.join(', ')}`,
     async () => {
+      // Turn platforms into a single array containing all the required configs
       const eslintrcExtends = platforms.reduce(
         (accumulator, platform) =>
           accumulator.concat(platformsConfigs[platform].eslintrc),
@@ -73,14 +101,30 @@ const writeEslintrc = platforms =>
     }
   );
 
+/**
+ * Gets the NPM dependencies for each platforms. It avoids duplicates
+ *
+ * @param  {Array<String>} platforms Platforms to be used to get deps from.
+ * @returns {Array<String} dependencies without duplicates.
+ */
+const getDepsForSelectedPlatforms = platforms => {
+  let deps = [];
+  for (const platform of platforms.values()) {
+    deps = deps.concat(platformsConfigs[platform].deps);
+  }
+  // Avoid duplicates creating a new set and expanding to an array
+  deps = [...new Set(deps)];
+  return deps;
+};
+
+/**
+ * Main function configure the dependencies and `.eslintrc` file based on the
+ * user input.
+ */
 const doRun = async () => {
   const { platforms, createEslintrc } = await askUserForConfigs();
 
-  let deps = [];
-  for (const platform of platforms.values()) {
-    deps = deps.concat(getPlatformDeps(platform));
-  }
-  deps = [...new Set(deps)]; // avoid duplicates
+  const deps = getDepsForSelectedPlatforms(platforms);
   await installDeps(deps);
 
   if (createEslintrc) {
@@ -88,6 +132,9 @@ const doRun = async () => {
   }
 };
 
+/**
+ * Message to be shown when everything has been executed successfully.
+ */
 const everythingSetUpMessage = () =>
   // eslint-disable-next-line no-console
   console.log(begoo('Everything Ready! Happy Coding!'));
