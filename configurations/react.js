47f6f3100da8bdf55d43271e42333907f0a8318a
Tomás Bustamante <tbustamante@atixlabs.com>
2020-02-05 13:03:34 +0000

    feat: added rules of hooks plugin

--- configurations/react.js
+++ configurations/react.js
@@ -3,5 +3,10 @@ module.exports = {
     'formidable/configurations/es6-react',
     'formidable/configurations/es6-test',
     './common.js'
-  ]
+  ],
+  plugins: ['react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
